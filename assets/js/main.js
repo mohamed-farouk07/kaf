bb1b24e8005067b820793668008214839881d8da
mohamed-farouk07 <faroukcr06@gmail.com>
2024-02-12 16:57:37 +0200

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,36 @@
 function toggleElementClass(target, elClass, htmlClass) {
-    target = document.querySelector(target);
-    if (target) {
-        if (target.classList.contains(elClass)) {
-            target.classList.remove(elClass);
-            document.documentElement.classList.remove(htmlClass);
-        } else {
-            target.classList.add(elClass);
-            document.documentElement.classList.add(htmlClass);
+  target = document.querySelector(target);
+  if (target) {
+    if (target.classList.contains(elClass)) {
+      target.classList.remove(elClass);
+      document.documentElement.classList.remove(htmlClass);
+    } else {
+      target.classList.add(elClass);
+      document.documentElement.classList.add(htmlClass);
+    }
+  }
+}
+
+window.onload = () => {
+  $(".dropdown-menu").on("click", function (event) {
+    var events = $._data(document, "events") || {};
+    events = events.click || [];
+    for (var i = 0; i < events.length; i++) {
+      if (events[i].selector) {
+        //Check if the clicked element matches the event selector
+        if ($(event.target).is(events[i].selector)) {
+          events[i].handler.call(event.target, event);
         }
 
+        // Check if any of the clicked element parents matches the
+        // delegated event selector (Emulating propagation)
+        $(event.target)
+          .parents(events[i].selector)
+          .each(function () {
+            events[i].handler.call(this, event);
+          });
+      }
     }
-}
+    event.stopPropagation(); //Always stop propagation
+  });
+};
