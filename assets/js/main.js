142df2083dd4c48a637a0ea9d47831cb4e8f0951
mohamed-farouk07 <faroukcr06@gmail.com>
2024-02-13 17:05:33 +0200

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,71 @@ window.onload = () => {
     event.stopPropagation(); //Always stop propagation
   });
 };
+
+
+var like_button = document.getElementById("like-button");
+if (like_button) {
+    like_button.addEventListener("click", doLikeButton);
+}
+
+function doLikeButton(e) {
+    toggleButton(e.target);
+}
+
+function toggleButton(button) {
+    button.classList.remove('liked-shaked');
+    button.classList.toggle('liked');
+    button.classList.toggle('not-liked');
+    button.classList.toggle('fa-heart');
+    button.classList.toggle('fa-heart');
+
+    if(button.classList.contains("liked")) {
+        button.classList.add('liked-shaked');
+    }
+}
+
+
+const counters = document.querySelectorAll('[data-counter]');
+if (counters) {
+  counters.forEach((counter) => {
+    counter.addEventListener('click', (e) => {
+      const target = e.target;
+      if (target.closest('.counter__btn')) {
+        let value = parseInt(
+          target.closest('.counter').querySelector('input').value
+        );
+        if (target.classList.contains('counter__btn--plus')) {
+          value++;
+        } else {
+          --value;
+        }
+        // ---- ---- 0 or another number ---- ---- //
+        if (value <= 0) {
+          value = 0;
+          target
+            .closest('.counter')
+            .querySelector('.counter__btn--minus')
+            .classList.add('disabled');
+        } else {
+          target
+            .closest('.counter')
+            .querySelector('.counter__btn--minus')
+            .classList.remove('disabled');
+        }
+        if (value >= 9) {
+          value = 9;
+          target
+            .closest('.counter')
+            .querySelector('.counter__btn--plus')
+            .classList.add('disabled');
+        } else {
+          target
+            .closest('.counter')
+            .querySelector('.counter__btn--plus')
+            .classList.remove('disabled');
+        }
+        target.closest('.counter').querySelector('input').value = value;
+      }
+    });
+  });
+}
